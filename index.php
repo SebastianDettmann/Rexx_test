ccf5ea3187b91355f7637d86633fb428a7fb5c92
SebastianDettmann <sebastian_dettmann@wtnet.de>
2019-01-25 00:31:47 +0100

    zwischenspeichern

--- index.php
+++ index.php
@@ -9,16 +9,18 @@
         <h1 align="center">Formular für die Erfassung von Kundenumsätzen in einem bestimmten Zeitraum</h1>
         <hr />
     </header>
-    <section align="center">
-        <p>Bitte geben Sie Start- und Enddatum ein. Anschließend wird eine csv Datei mit relevanten Kundendaten für diesen Zeitraum erstellt.</p>
-        <hr />
-        <form method="post" action="create_csv.php" >
-            <label for="start">Startdatum</label>
-            <input type="date" name="start" required>
-            <label for="start">Enddatum</label>
-            <input type="date" name="end" required>
-            <input type="submit">
-        </form>
+    <section>
+        <div align="center">
+            <p>Bitte geben Sie Start- und Enddatum ein. Anschließend wird eine csv Datei mit relevanten Kundendaten für diesen Zeitraum erstellt.</p>
+            <hr />
+            <form method="post" action="create_csv.php">
+                <label for="start">Startdatum</label>
+                <input type="date" name="start" id="start" required>
+                <label for="start">Enddatum</label>
+                <input type="date" name="end" id="start" required>
+                <input type="submit">
+            </form>
+        </div>
     </section>
 </body>
 </html>
